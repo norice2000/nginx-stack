00f53852606a28f6b80553b6e81b6c2ab76abbaf
Phong <phongmchau@gmail.com>
2024-09-21 09:04:15 +1000

    updated tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,32 @@
-# main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"  # or your preferred region
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-0ebfd941bbafe70c6"  # Amazon Linux 2023 AMI ID for us-east-1
+}
+
+variable "key_name" {
+  default = "keypair"  # The name of your existing key pair in AWS
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-0ebfd941bbafe70c6"  # Amazon Linux 2023 AMI ID for us-east-1
+  ami           = var.ami_id
   instance_type = "t3.micro"
-  key_name      = "keypair"
+  key_name      = var.key_name  # Use the existing key pair
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
@@ -14,31 +34,21 @@ resource "aws_instance" "web_server" {
     Name = "WebServer"
   }
 
-  # Wait for instance to be ready
   provisioner "remote-exec" {
     inline = ["echo 'Instance is ready'"]
 
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")
+      private_key = file("~/.ssh/keypair.pem")  # Adjust this path to where you've stored the downloaded private key
       host        = self.public_ip
     }
   }
-  
-  # Install Ansible
-  provisioner "local-exec" {
-    command = "python3 -m pip install --no-cache-dir ansible==8.7.0"
-  }
-  # Run Ansible playbook
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user -i '${self.public_ip},' --private-key ~/.ssh/id_rsa ${path.root}/../ansible/main.yml"
-  }
-}
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")  # Make sure this key exists
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user -i '${self.public_ip},' --private-key ~/.ssh/keypair.pem ${path.root}/../ansible/main.yml"
+    on_failure = continue
+  }
 }
 
 resource "aws_security_group" "web_sg" {
